0ff80bf2dde22c948af0c2547eefc8bec5e60974
p0d00td <prasad.dola@walmart.com>
2023-06-08 15:51:10 +0530

    fix bugs

--- src/api/index.js
+++ src/api/index.js
@@ -3,5 +3,5 @@ require('dotenv').config()
 
 const app = express()
 const port = 4000
-app.get("/health", (req, res) => res.send({ "meesage": "APP1 200 OK testing"}) )
+app.get("/health", (req, res) => res.send({ "meesage": "APP1 200 OK real"}) )
 app.listen(port, () => console.log(`Example backend API listening on port ${port}!`))
